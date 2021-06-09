6c792164383dac0f032d8a3df78458b0b2b5faba
victor kigen <vickigen@gmail.com>
2021-06-09 15:23:59 +0300

    added profile

--- repair/admin.py
+++ repair/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Repair,Subject,Profile
 # Register your models here.
+
+admin.site.register(Repair)
+admin.site.register(Subject)
+admin.site.register(Profile)
