b176b3f6793b046cb55a8abf93572372e83defad
victor kigen <vickigen@gmail.com>
2021-06-09 15:24:37 +0300

    added user register form

--- repair/forms.py
+++ repair/forms.py
@@ -1,7 +1,22 @@
-from .models import Repair
+from .models import Repair, Profile
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 # from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_photo','contact','bio']
 
 
 class RepairForm(forms.ModelForm):
