c2716228246c78faeaaf10b4405ede0e653f2c5f
victor kigen <vickigen@gmail.com>
2021-06-09 15:26:06 +0300

    added profile class

--- repair/models.py
+++ repair/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
-
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,3 +19,34 @@ class Repair(models.Model):
     
     def __str__(self):
         return self.title
+
+class Profile(models.Model):
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField()
+    contact = models.CharField(max_length=60)
+    user = models.OneToOneField(
+                User,
+                on_delete=models.CASCADE,
+            )
+
+    def save_profile(self):
+        return self.save()
+
+
+    @classmethod
+    def update_profile(cls,update_details,id):
+        return cls.objects.filter(id=int(id)).update(profile_photo=update_details['profile_photo'],
+                                               bio=update_details['bio'],
+                                               contact= update_details['contact'],
+                                               user=update_details['user'])
+
+    def delete_profile(self):
+        return self.delete()
+
+
+    class Meta:
+        ordering = ["-pk"]
+
+
+    def __str__(self):
+        return f'{self.user.username}'
